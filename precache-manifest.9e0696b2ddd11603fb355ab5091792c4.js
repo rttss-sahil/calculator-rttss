5eb1f1a0e91e57f001dc41b5868f910eaaab4970
rttss <sahil.git@outlook.com>
2020-10-06 04:29:40 +0530

    react build

---
+++ precache-manifest.9e0696b2ddd11603fb355ab5091792c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53babf369a9300765789e1f207361a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "646f44614f5bf2078ca2",
+    "url": "/static/css/main.ec11cad0.chunk.css"
+  },
+  {
+    "revision": "6134e00eaa55b744992c",
+    "url": "/static/js/2.0c484f4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c484f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646f44614f5bf2078ca2",
+    "url": "/static/js/main.94f1c3fa.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
